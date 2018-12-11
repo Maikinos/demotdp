5e1e2dac7b9ed9763793ab095f0c2ab343f90bd5
Michael Vairo <t.csti.mvairo@quimicasuiza.com>
2018-12-11 16:11:37 -0500

    Auto-generated commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 //new WOW().init();
-function openCity(cityName,elmnt) { 
+function openCity(cityName, elmnt) {
 
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -16,75 +16,77 @@ function openCity(cityName,elmnt) {
     elmnt.style.backgroundColor = '#f52d2d';
     elmnt.style.color = '#ffff';
 
-    
+
 }
 
-function oTab(){
-    $( "#check-prod" ).click(function() {
+function oTab() {
+    $("#check-prod").click(function () {
         $("#l-pedido").click();
-      });
+    });
 }
 
-function ontaber(){
+function ontaber() {
     $(".promo").show();
 }
 
-function traeMe(){
-    $('#buscar').on('click', function(){
+function traeMe() {
+    $('#buscar').on('click', function () {
 
-    
+
         compruebaim = $('#txtbuscar').val();
-    
+
         pat1 = /actrim/;
         pat2 = /epriman/;
-            
+
         siinst = pat1.test(compruebaim);
         siinst2 = pat2.test(compruebaim);
-    
+
         if (siinst) {
             $('.returna-cos').show();
             $('.returna-bactrim').show();
             $('.returna-repriman').hide();
         }
-    
-        else if(siinst2){
+
+        else if (siinst2) {
             $('.returna-cos').show();
             $('.returna-bactrim').hide();
             $('.returna-repriman').show();
         }
-        
+
     });
 }
 
-function agregadoSelecto(){
-    $('.prod-selec').on('click', function(){
+function agregadoSelecto() {
+    $('.prod-selec').on('click', function () {
 
         $('.returna-wos').show();
         $('.ccptt01').show();
-       
-        
+
+
     });
 
-    $('.prod-selec2').on('click', function(){
+    $('.prod-selec2').on('click', function () {
 
         $('.returna-wos').show();
         $('.ccptt02').show();
-       
-        
+
+
     });
 }
 
 //Toggle chevrons
 function toggleChevron(e) {
-$(e.target)
-.prev('.panel-heading')
-.find("i.indicator")
-.toggleClass('glyphicon-chevron-down-custom glyphicon-chevron-up-custom');
-        }
-        $('#accordion').on('hidden.bs.collapse', toggleChevron);
-        $('#accordion').on('shown.bs.collapse', toggleChevron);
-
-        
+    $(e.target)
+        .prev('.panel-heading')
+        .find("i.indicator")
+        .toggleClass('glyphicon-chevron-down-custom glyphicon-chevron-up-custom');
+}
+$('#accordion').on('hidden.bs.collapse', toggleChevron);
+$('#accordion').on('shown.bs.collapse', toggleChevron);
 
 
-        
+
+
+
+
+
